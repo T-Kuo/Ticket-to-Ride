eaa783f19568e8b97b3d8d152c9a30fb2a8c79c4
bjc265 <bjc265@cornell.edu>
2016-11-09 17:34:52 -0500

    Fix board to be a proper .mli file

--- board.mli
+++ board.mli
@@ -1,32 +1,25 @@
-module Board = sig
+type player
+type color
+type city
+type route
 
-	type player = NoPlayer | Player1 | Player2 | Player3 | Player4 | Player5
-	type color = Colorless | Red | Blue | Yellow | Green | Orange | Black | Pink
-	type city = string
-	type route = {n0 : city; n2 : city; length : int; color : color; owner : player}
-
-	module City = struct
+module City : sig
 		type t = city
-		let compare n1 n2 = String.compare n1 n2
-	end
-
-	module BMap = Map.Make(City)
-
-	type board = ((route ref list) BMap.t)
-
-	val empty : board
-
-	val add_city : city -> board -> board
-
-	val add_route : route -> board -> board
-
-	val shortest_path : player -> city -> city -> board -> route list
-
-	val has_connected : player -> city -> city -> board -> bool
-
-	val claim_route : player -> route -> int -> bool * board
-
-	val routes_between : city -> city -> route list
-
-
+		val compare : city -> city -> int
 end
+
+type t
+
+val empty : t
+
+val add_city : city -> t -> t
+
+val add_route : route -> t -> t
+
+val shortest_path : player -> city -> city -> t -> route list
+
+val has_connected : player -> city -> city -> t -> bool
+
+val claim_route : player -> route -> int -> bool * t
+
+val routes_between : city -> city -> route list
