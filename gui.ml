277d46349e91ce89e8592fe9d975ac5eb4b80f64
Ubuntu <ubuntu@csvm>
2016-12-03 23:00:24 -0500

    Made stuff global in gui

--- gui.ml
+++ gui.ml
@@ -20,6 +20,22 @@ let select_c1 = ref (GEdit.combo ~popdown_strings:(filler ()) ())
 let colors = GEdit.combo
   ~popdown_strings: ["red";"blue";"yellow";"green";"orange";"pink";"white";
   "black"] ~width:150 ()
+let button0 = ref (GButton.button ())
+let button1 = ref (GButton.button ())
+let button2 = ref (GButton.button ())
+let button3 = ref (GButton.button ())
+let button4 = ref (GButton.button ())
+let button5 = ref (GButton.button ())
+let rainbow_label = ref (GMisc.label ())
+let red_label = ref (GMisc.label ())
+let blue_label = ref (GMisc.label ())
+let yellow_label = ref (GMisc.label ())
+let green_label = ref (GMisc.label ())
+let orange_label = ref (GMisc.label ())
+let pink_label = ref (GMisc.label ())
+let white_label = ref (GMisc.label ())
+let black_label = ref (GMisc.label ())
+let train_button = ref (GButton.button ~label:"70 trains" ())
 
 (* Global references to face up card objects *)
 
@@ -42,7 +58,7 @@ let match_jpg color =
   | Color.Yellow -> "images/yellow_card.jpg"
   | Color.Green -> "images/green_card.jpg"
   | Color.Orange -> "images/orange_card.jpg"
-  | Color.Pink -> "images/pink_card.jpg"
+  | Color.Pink -> "images/purple_card.jpg"
   | Color.White -> "images/white_card.jpg"
   | Color.Black -> "images/black_card.jpg"
 
@@ -53,7 +69,7 @@ let main_gui () =
   ignore(window#connect#destroy ~callback: GMain.Main.quit);
   let bigbox = GPack.hbox ~packing:window#add ~border_width:5 () in
   let deck = GPack.table ~rows:6 ~columns:1 ~homogeneous:true
-      ~packing:(bigbox#pack ~from: `END) () in
+    ~packing:(bigbox#pack ~from: `END) () in
   let claims = GPack.table ~rows:3 ~columns:1 ~homogeneous:false
   ~packing:(bigbox#pack ~from: `END) () in
   let bigtable = GPack.table ~rows:2 ~columns:1 ~width: 900 ~homogeneous:false
@@ -75,99 +91,106 @@ let main_gui () =
   claims_table#attach !select_c0#coerce ~left:0 ~top:0;
   claims_table#attach !select_c1#coerce ~left:1 ~top:0;
   claims_table#attach colors#coerce ~left:2 ~top:0;
-  let train_button = GButton.button ~label:"45 trains" () in
-  claims#attach ~left:0 ~top:3 (train_button#coerce);
-  let button1 = GButton.button () in
-  deck#attach ~left:0 ~top:0 (button1#coerce);
-  ignore(button1#connect#clicked ~callback:(fun () -> prerr_endline "button 1"));
-  let button2 = GButton.button () in
-  deck#attach  ~left:0 ~top:1 (button2#coerce);
-  ignore(button2#connect#clicked ~callback:(fun () -> prerr_endline "button 2"));
-  let button3 = GButton.button () in
-  deck#attach  ~left:0 ~top:2 (button3#coerce);
-  ignore(button3#connect#clicked ~callback:(fun () -> prerr_endline "button 3"));
-  let button4 = GButton.button () in
-  deck#attach  ~left:0 ~top:3 (button4#coerce);
-  ignore(button4#connect#clicked ~callback:(fun () -> prerr_endline "button 4"));
-  let button5 = GButton.button () in
-  deck#attach  ~left:0 ~top:4 (button5#coerce);
-  ignore(button5#connect#clicked ~callback:(fun () -> prerr_endline "button 5"));
-  let button6 = GButton.button () in
-  deck#attach  ~left:0 ~top:5 (button6#coerce);
-  ignore(button5#connect#clicked ~callback:(fun () -> prerr_endline "button 6"));
+  claims#attach ~left:0 ~top:3 (!train_button#coerce);
+
+  button0 := GButton.button ();
+  deck#attach ~left:0 ~top:0 (!button0#coerce);
+  ignore(!button0#connect#clicked ~callback:(fun () -> draw0 := true;
+    prerr_endline "button 1"));
+  button1 := GButton.button ();
+  deck#attach  ~left:0 ~top:1 (!button1#coerce);
+  ignore(!button1#connect#clicked ~callback:(fun () -> draw1 := true;
+    prerr_endline "button 2"));
+  button2 := GButton.button ();
+  deck#attach  ~left:0 ~top:2 (!button2#coerce);
+  ignore(!button2#connect#clicked ~callback:(fun () -> draw2 := true;
+    prerr_endline "button 3"));
+  button3 := GButton.button ();
+  deck#attach  ~left:0 ~top:3 (!button3#coerce);
+  ignore(!button3#connect#clicked ~callback:(fun () -> draw3 := true;
+    prerr_endline "button 4"));
+  button4 := GButton.button ();
+  deck#attach  ~left:0 ~top:4 (!button4#coerce);
+  ignore(!button4#connect#clicked ~callback:(fun () -> draw4 := true;
+    prerr_endline "button 5"));
+  button5 := GButton.button ();
+  deck#attach  ~left:0 ~top:5 (!button5#coerce);
+  ignore(!button5#connect#clicked ~callback:(fun () -> draw_deck := true;
+    prerr_endline "button 6"));
+
   let _img1 = GMisc.image ~file:"../TtR/images/black_card.jpg"
-  ~packing: button1#add () in
+  ~packing: !button0#add () in
 
   let _img2 = GMisc.image ~file:"../TtR/images/black_card.jpg"
-  ~packing: button2#add () in
+  ~packing: !button1#add () in
 
   let _img3 = GMisc.image ~file:"../TtR/images/black_card.jpg"
-  ~packing: button3#add () in
+  ~packing: !button2#add () in
 
   let _img4 = GMisc.image ~file:"../TtR/images/black_card.jpg"
-  ~packing: button4#add () in
+  ~packing: !button3#add () in
 
   let _img5 = GMisc.image ~file:"../TtR/images/black_card.jpg"
-  ~packing: button5#add () in
+  ~packing: !button4#add () in
 
   let _img6 = GMisc.image ~file:"../TtR/images/deck_card.jpg"
-  ~packing: button6#add () in
+  ~packing: !button5#add () in
 
   let hand_row = GPack.table ~rows:3 ~columns:6 ~homogeneous:true () in
   bigtable#attach ~left:0 ~top:1 (hand_row#coerce);
 
   let rainbow_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/rainbow_card.jpg"  ~packing:rainbow_box#pack () in
-  let _label = GMisc.label ~text: "3" ~markup:"<span foreground=\"black\" size = \"x-large\">2</span>"
-  ~packing:rainbow_box#pack() in
+  rainbow_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:rainbow_box#pack ();
   hand_row#attach ~left: 1 ~top:1 (rainbow_box#coerce);
 
   let red_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/red_card.jpg"  ~packing:red_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">3</span>"
-  ~packing:red_box#pack() in
+  red_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:red_box#pack ();
   hand_row#attach ~left: 2 ~top:1 (red_box#coerce);
 
   let blue_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/blue_card.jpg"  ~packing:blue_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">6</span>"
-  ~packing:blue_box#pack() in
+  blue_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:blue_box#pack ();
   hand_row#attach ~left: 3 ~top:1 (blue_box#coerce);
 
   let yellow_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/yellow_card.jpg"  ~packing:yellow_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">6</span>"
-  ~packing:yellow_box#pack() in
+  yellow_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:yellow_box#pack ();
   hand_row#attach ~left: 4 ~top:1 (yellow_box#coerce);
 
   let green_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/green_card.jpg"  ~packing:green_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">6</span>"
-  ~packing:green_box#pack() in
+  green_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:green_box#pack ();
   hand_row#attach ~left: 1 ~top:2 (green_box#coerce);
 
   let orange_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/orange_card.jpg"  ~packing:orange_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">6</span>"
-  ~packing:orange_box#pack() in
+  orange_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:orange_box#pack ();
   hand_row#attach ~left: 2 ~top:2 (orange_box#coerce);
 
   let pink_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/purple_card.jpg"  ~packing:pink_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">6</span>"
-  ~packing:pink_box#pack() in
+  pink_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:pink_box#pack ();
   hand_row#attach ~left: 3 ~top:2 (pink_box#coerce);
 
   let white_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/white_card.jpg"  ~packing:white_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">6</span>"
-  ~packing:white_box#pack() in
+  white_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:white_box#pack ();
   hand_row#attach ~left: 4 ~top:2 (white_box#coerce);
 
   let black_box = GPack.hbox () in
   let _img6 = GMisc.image ~file:"../TtR/images/black_card.jpg"  ~packing:black_box#pack () in
-  let _label = GMisc.label ~text: "5" ~markup:"<span foreground=\"black\" size = \"x-large\">6</span>"
-  ~packing:black_box#pack() in
+  black_label := GMisc.label ~markup:"<span foreground=\"black\" size =
+  \"x-large\">0</span>" ~packing:black_box#pack ();
   hand_row#attach ~left: 5 ~top:2 (black_box#coerce);
 
   window#show ();
